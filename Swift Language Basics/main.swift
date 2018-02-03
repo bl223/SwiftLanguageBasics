b713a937547512bd465aa88745079f562e34d836
Bryce Ligaya <bryceligaya@Bryces-MacBook-Air.local>
2018-02-02 23:25:08 -0600

    Applied .gitignore

---
+++ Swift Language Basics/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Bryce Ligaya on 2/2/18.
+//  Copyright © 2018 Bryce Ligaya. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let sample1:            = 0x3A
+var sample2             = 58
+var heartrate: Int      = 85
+var deposits: Double    = 135002796
+var acceleration: Float = 9.800
+var mass: Float         = 14.6
+var distance: Double    = 129.763001
+var lost: Bool          = true
+var choice: Int         = 2
+let integral: Char      = \u{222B}
+var greeting: String    = Hello
+var name:   String      = Karen
+
